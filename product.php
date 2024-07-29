b02ece2c565fc3811e34d3ce6225df00006c9fad
elitetriceratops123 <alirajput3675@gmail.com>
2024-07-29 06:24:07 +0500

    update product.php

--- product.php
+++ product.php
@@ -30,14 +30,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['customer-email'];
     $phone = $_POST['customer-phone'];
     $address = $_POST['address'];
-    
+
     // SQL query to insert data into the table
     $sql = "INSERT INTO customers (first_name, last_name, email, phone, address)
             VALUES ('$firstName', '$lastName', '$email', '$phone', '$address')";
 
     if (mysqli_query($conn, $sql)) {
         echo "New record created successfully";
-        
+
         // Send email using PHPMailer
         $mail = new PHPMailer(true);
 
@@ -45,25 +45,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             //Server settings
             $mail->SMTPDebug = 0;                                       // Enable verbose debug output
             $mail->isSMTP();                                            // Set mailer to use SMTP
-            $mail->Host       = 'smtp.gmail.com';                     // Specify main and backup SMTP servers
-            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'za986137@gmail.com';               // SMTP username
-            $mail->Password   = 'arqi kihe bwjh pfaf';                  // SMTP password
+            $mail->Host = 'smtp.gmail.com';                     // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                                   // Enable SMTP authentication
+            $mail->Username = '#';               // SMTP username
+            $mail->Password = '#git';                  // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption, `PHPMailer::ENCRYPTION_SMTPS` also accepted
-            $mail->Port       = 587;                                    // TCP port to connect to
+            $mail->Port = 587;                                    // TCP port to connect to
 
             //Recipients
-            $mail->setFrom('za986137@gmail.com', 'Fragrant');
+            $mail->setFrom('#', 'Fragrant');
             $mail->addAddress($email, $firstName . ' ' . $lastName);    // Add a recipient
 
             // Content
             $mail->isHTML(true);                                        // Set email format to HTML
             $mail->Subject = 'Thank you for registering';
-            $mail->Body    = 'Dear ' . $firstName . ' ' . $lastName . ',<br><br>Thank you for registering. We have received your details and will get back to you soon.<br><br>Regards,<br>Perfume Store';
+            $mail->Body = 'Dear ' . $firstName . ' ' . $lastName . ',<br><br>Thank you for registering. We have received your details and will get back to you soon.<br><br>Regards,<br>Fragrant';
             $mail->AltBody = 'Dear ' . $firstName . ' ' . $lastName . ',\n\nThank you for registering. We have received your details and will get back to you soon.\n\nRegards,\nPerfume Store';
 
-            $mail->send();
-            echo 'Message has been sent';
+            if ($mail->send()) {
+                echo "<script>alert('Order Confirmed')</script>";
+                echo "<script>window.locatoin = 'index.html'</script>";
+            }
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
